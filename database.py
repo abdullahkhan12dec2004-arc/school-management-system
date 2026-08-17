8b4b080b95937733f0da7854d694d7c48f23a024
abdullahkhan12dec2004-arc <abdullahkhan12dec2004@gmail.com>
2026-08-17 12:56:38 +0500

    Update database.py

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ import psycopg2.extras
 
 
 DB_CONFIG = {
-    'host': os.environ.get('DB_HOST'),
+    'host': os.environ.get('DB_HOST').strip(),
     'port': os.environ.get('DB_PORT', '5432'),
     'dbname': os.environ.get('DB_NAME'),
     'user': os.environ.get('DB_USER'),
