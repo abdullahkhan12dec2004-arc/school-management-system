daa3c752237b07febb87f0214a62a73cbcc9b571
abdullahkhan12dec2004-arc <abdullahkhan12dec2004@gmail.com>
2026-09-11 16:47:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 app.py - Flask Application Routes
 School Management System with Role-Based Access
 """
-
-
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 
 from flask import request, redirect, url_for, flash, session, send_file
